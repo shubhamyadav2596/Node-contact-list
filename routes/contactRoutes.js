fce7fc070ff2fe8f0349e14e6df875ac09820edc
progammerBoy <sy8692935@gmail.com>
2026-04-10 16:15:16 +0530

    update route file

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -6,10 +6,7 @@ const contactController = require('../controllers/contactController');
 const upload = multer({ storage: multer.memoryStorage() });
 
 router.post('/upload', upload.any(), (req, res) => {
-    console.log("==== UPLOAD DEBUG INFO ====");
-    console.log("1. Files jo server tak aayi:", req.files);
-    console.log("2. Text fields jo server tak aaye (Agar body mein kuch extra hai):", req.body);
-    console.log("===========================");
+  
 
     if (req.files && req.files.length > 0) {
         req.file = req.files[0]; 
